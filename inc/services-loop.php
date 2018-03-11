4673a3ebcb7cdd8186de9a75ff52be51011c7bf7
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-11 14:10:38 -0600

    Update services-loop.php

--- inc/services-loop.php
+++ inc/services-loop.php
@@ -44,7 +44,7 @@
     </div>
     <!--col-->
 </div> 
-		  <?php $counter++; if($counter % 4 === 0) :  echo '</div> <div class="row">'; endif; ?>
+		  <?php $counter++; if($counter % 4 === 0) :  echo '</div> <div style="margin-top:40px;" class="row">'; endif; ?>
 	  
 								
 						<?php endwhile; endif; ?>	
