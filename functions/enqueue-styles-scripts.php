7c893b5503179431e4e6dd168458b4888287d095
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-11 10:54:38 -0600

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -3,22 +3,21 @@
 	function bsc_enqueue_styles_scripts() {
 		
 		// CSS
-				
-		wp_enqueue_style('style', get_template_directory_uri() . '/css/custom-layout.css');
+		$stylesheets = array("bootstrap/bootstrap.min","animation","button","buttons","custom-layout","fonts","headings","mobile","preloader","special-classes","typography");
 		
+		foreach ($stylesheets as $stylesheet) {
+    		wp_enqueue_style($stylesheet, get_template_directory_uri() . '/css/'.$stylesheet.'.css');
+		}
+						
 
 		// JS
-		
-		wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array(), null, true ); 
-		
-		//wp_enqueue_script( 'min', get_template_directory_uri() . '/js/jquery-1.12.3.min.js', array(), null, true );
-		wp_enqueue_script( 'jquery'); 
-
-		wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/jquery.animate-enhanced.min.js', array(), null, true ); 
-		
-		wp_enqueue_script( 'easypiechart', get_template_directory_uri() . '/js/jquery.easypiechart.min.js', array(), null, true ); 
-		
-		wp_enqueue_script( 'unveil', get_template_directory_uri() . '/js/jquery.unveilEffects.js', array(), null, true ); 
+		//wp_enqueue_script( 'jquery');
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/jquery-1.12.3.min.js',$deps, $ver, false); 
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/bootstrap/bootstrap.js',$deps, $ver, true); 
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/jquery.unveilEffects.js',$deps, $ver, true); 
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/owl-carousel/owl.carousel.min.js',$deps, $ver, true); 
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/projects/isotope.js',$deps, $ver, true); 
+		wp_enqueue_style('style', get_template_directory_uri() . '/js/custom.js',$deps, $ver, true);
 
 	} 
 		
