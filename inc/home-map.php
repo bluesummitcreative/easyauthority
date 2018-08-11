533f6af8889abbb6d620605b2f597e2378bc5422
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-11 11:23:41 -0600

    Update home-map.php

--- inc/home-map.php
+++ inc/home-map.php
@@ -5,22 +5,7 @@
       <h2>Send a message</h2>
      <div id="form_result"></div>
           <div class="clear"></div>
-          <form method="post" id="contactpage">
-            <input type="hidden" name="to" value="support@easyauthority.com"/>
-      <ul>
-        <li>
-          <input name="name" id="name" type="text" placeholder="Name:" />
-        </li>
-        <li>
-         <input name="email" id="email" type="text" placeholder="Email:" />
-        </li>
-        <li>
-         <input name="comments" id="comments" type="text" placeholder="Message:" />
-        </li>
-      </ul>
-      <button class="light-font" name="send" type="submit">send message Now <img alt="" src="/wp-content/themes/easyauthority/images/arrow-white-right.png"></button>
-      </form>
-      <!--msg-form--> 
+          <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="10"]'); ?>
     </div>
     <div id="back-top"><a href="#top"><img src="/wp-content/themes/easyauthority/images/arrow-white-top.png" alt="" /> <span>BA<br>
       CK</span></a></div>
