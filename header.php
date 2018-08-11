ce88f224adca67086ad01c71359b7b775b792630
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-11 10:51:32 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,18 +1,11 @@
 <!doctype html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="/wp-content/themes/easyauthority/css/all-stylesheets.css" type="text/css" />
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-<!-- Fav and touch icons -->
-</head>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<?php wp_head(); ?>
+	</head>
 <body class="home-page">
 <div class="page-loader-wrapper page-loader-wrapper-1">
   <div id="preloader_1"> <span></span> <span></span> <span></span> <span></span> <span></span> 
