546bd2e757be6cb5e3d79077b593bacd584a8568
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-11 10:59:00 -0600

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -3,7 +3,7 @@
 	function bsc_enqueue_styles_scripts() {
 		
 		// CSS
-		$stylesheets = array("bootstrap/bootstrap.min","animation","button","buttons","fonts","headings","mobile","preloader","special-classes","typography","custom-layout");
+		$stylesheets = array("bootstrap/bootstrap.min","animation","button","buttons","custom-layout","fonts","headings","mobile","preloader","special-classes","typography");
 		
 		foreach ($stylesheets as $stylesheet) {
     		wp_enqueue_style($stylesheet, get_template_directory_uri() . '/css/'.$stylesheet.'.css');
@@ -12,12 +12,12 @@
 
 		// JS
 		//wp_enqueue_script( 'jquery');
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/jquery-1.12.3.min.js',$deps, $ver, false); 
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/bootstrap/bootstrap.js',$deps, $ver, true); 
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/jquery.unveilEffects.js',$deps, $ver, true); 
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/owl-carousel/owl.carousel.min.js',$deps, $ver, true); 
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/projects/isotope.js',$deps, $ver, true); 
-		wp_enqueue_style('style', get_template_directory_uri() . '/js/custom.js',$deps, $ver, true);
+		wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-1.12.3.min.js',$deps, $ver, false); 
+		wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap/bootstrap.js',$deps, $ver, true); 
+		wp_enqueue_script('enveilEffects', get_template_directory_uri() . '/js/jquery.unveilEffects.js',$deps, $ver, true); 
+		wp_enqueue_script('owl-carousel', get_template_directory_uri() . '/js/owl-carousel/owl.carousel.min.js',$deps, $ver, true); 
+		wp_enqueue_script('isotope', get_template_directory_uri() . '/js/projects/isotope.js',$deps, $ver, true); 
+		wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js',$deps, $ver, true);
 
 	} 
 		
