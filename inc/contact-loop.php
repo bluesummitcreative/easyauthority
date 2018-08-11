18fd9686ef5db2fe62bcec746581ca7f0756df7e
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-11 11:40:09 -0600

    Update contact-loop.php

--- inc/contact-loop.php
+++ inc/contact-loop.php
@@ -8,22 +8,7 @@
       <h2>Send a message</h2>
        <div id="form_result"></div>
           <div class="clear"></div>
-          <form method="post" id="contactpage">
-            <input type="hidden" name="to" value="name@easyauthority.com"/>
-      <ul>
-        <li>
-          <input name="name" id="name" type="text" placeholder="Name:" />
-        </li>
-        <li>
-         <input name="email" id="email" type="text" placeholder="Email:" />
-        </li>
-        <li>
-         <input name="comments" id="comments" type="text" placeholder="Message:" />
-        </li>
-      </ul>
-      <button class="light-font" name="send" type="submit">send message Now <img alt="" src="images/arrow-white-right.png"></button>
-      </form>
-      <!--msg-form--> 
+          <?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true" tabindex="10"]'); ?>
     </div>
      <aside class="contact-info col-lg-6 col-md-6 col-sm-12 col-xs-12">
       
